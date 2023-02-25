04d9c1ab70aade2b521b3afb8fdf5bcc5bad57f8
samanthemum <sambhallam@gmail.com>
2023-02-24 22:05:13 -0600

    Added attack animation and controls

---
+++ A Part/Assets/Scripts/Attack.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float attackDamage = 10;
+    [SerializeField] float currentCoolDownTime = .0f;
+    [SerializeField] float attackCoolDown = .5f;
+    [SerializeField] Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Attack if button pressed and cool down is ready
+        if(Input.GetAxis("Attack") > 0 && currentCoolDownTime == 0.0f)
+        {
+            animator.SetTrigger("attack");
+            currentCoolDownTime = attackCoolDown;
+            Debug.Log("You attacked");
+        }
+
+
+        // Refresh cooldown
+        if(currentCoolDownTime > 0)
+        {
+            currentCoolDownTime -= Time.deltaTime;
+            currentCoolDownTime = currentCoolDownTime < 0 ? 0 : currentCoolDownTime;
+        }
+    }
+}
